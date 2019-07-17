73b7afecb11322c5b3afc12bf9ae4b70d341da38
Blake Ba <blake.elink@gmail.com>
2019-07-17 19:08:55 -0400

    testing card hand capacity

--- Hand.cs
+++ Hand.cs
@@ -6,7 +6,13 @@ namespace PokerConsoleApp
 {
     class Hand
     {
-        private List<Card> card = new List<Card>();
-        
+        private List<Card> cards = new List<Card> { };
+        public Hand()
+        {
+            Console.WriteLine($"hand capacity = {cards.Capacity}");
+            cards.Capacity = 5;
+            Console.WriteLine($"hand capacity = {cards.Capacity}");
+
+        }
     }
 }
